b6b0b067c64fd020104add4b83ee9ceb5920940a
ChaimaaLachhab <chaimaalachhab555@gmail.com>
2025-03-11 16:32:02 +0000

    Finished Exercise 6

---
+++ Week-1/Day-2/exercises/exercisese_6.py
@@ -0,0 +1,9 @@
+#🌟 Exercise 6 : Let’s create some personalized shirts !
+
+def make_shirt(size="large", message="I love Python"):
+    print(f"The size of the shirt is {size} and the text is '{message}'.")
+
+make_shirt()
+make_shirt(size="medium")
+make_shirt(size="small", message="Hello, World!")
+make_shirt(message="Custom message", size="extra-large")
