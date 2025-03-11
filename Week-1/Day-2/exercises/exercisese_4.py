0700e868bb386139405046095e34f392e6fdb631
ChaimaaLachhab <chaimaalachhab555@gmail.com>
2025-03-11 16:03:12 +0000

    Finished Exercise 4

---
+++ Week-1/Day-2/exercises/exercisese_4.py
@@ -0,0 +1,11 @@
+# 🌟 Exercise 4 : Some Geography
+
+def describe_city(city, country ="Morocco"):
+    print(city +" is in "+ country)
+    # OR
+    print(f"{city} is in {country}.")
+
+
+describe_city("Rabat")
+describe_city("New York", "USA")
+describe_city("Tokyo", "Japan")
