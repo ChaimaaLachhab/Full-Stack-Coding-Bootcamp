3b3c3cb52efa5e0463e56ae8dcd205f0f6a25117
ChaimaaLachhab <chaimaalachhab555@gmail.com>
2025-03-25 14:35:33 +0000

    Finished daily challenge 2 week 3 day 3

--- Week-3/Day-3/daily_challenge/challenge_2/script.js
+++ Week-3/Day-3/daily_challenge/challenge_2/script.js
@@ -1,16 +1,11 @@
-// Demande à l'utilisateur d'entrer des mots séparés par des virgules
 const userInput = prompt("Entrez plusieurs mots séparés par des virgules :");
 
-// Transforme l'entrée en un tableau en supprimant les espaces inutiles
 const words = userInput.split(",").map(word => word.trim());
 
-// Trouve la longueur du mot le plus long
 const maxLength = Math.max(...words.map(word => word.length));
 
-// Crée la ligne de bordure
 const border = "*".repeat(maxLength + 4);
 
-// Affiche le cadre dans la console
 console.log(border);
 words.forEach(word => {
     console.log(`* ${word.padEnd(maxLength)} *`);
